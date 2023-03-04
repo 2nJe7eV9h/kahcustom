0bdb5e4313a209a75638bcd2dc8d50495a1db20f
AnonymousCoder <76236098+2nJe7eV9h@users.noreply.github.com>
2023-03-04 22:22:43 +0000

    Update FPS.lua

--- FPS.lua
+++ FPS.lua
@@ -3,24 +3,20 @@
 
 -- Instances:
 
-local fps = Instance.new("ScreenGui")
 local holder = Instance.new("ImageLabel")
 local clock = Instance.new("ImageLabel")
 local label = Instance.new("TextLabel")
 
 --Properties:
 
-fps.Name = "fps"
-fps.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-
 holder.Name = "holder"
-holder.Parent = fps
+holder.Parent = game.StarterGui.fps
 holder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 holder.BackgroundTransparency = 1.000
-holder.Position = UDim2.new(0.38936165, 0, 0.930232525, 0)
-holder.Size = UDim2.new(0, 311, 0, 47)
+holder.Position = UDim2.new(0.428770512, 0, 0.943696439, 0)
+holder.Size = UDim2.new(0, 172, 0, 37)
 holder.Image = "rbxassetid://3570695787"
-holder.ImageColor3 = Color3.fromRGB(91, 77, 138)
+holder.ImageColor3 = Color3.fromRGB(51, 46, 73)
 holder.ScaleType = Enum.ScaleType.Slice
 holder.SliceCenter = Rect.new(100, 100, 100, 100)
 holder.SliceScale = 0.120
@@ -28,8 +24,8 @@ holder.SliceScale = 0.120
 clock.Name = "clock"
 clock.Parent = holder
 clock.BackgroundTransparency = 1.000
-clock.Position = UDim2.new(0.0353697762, 0, 0.106382981, 0)
-clock.Size = UDim2.new(0, 39, 0, 37)
+clock.Position = UDim2.new(0.0406976752, -1, 0.134424165, 0)
+clock.Size = UDim2.new(0, 27, 0, 27)
 clock.Image = "rbxassetid://2774790436"
 
 label.Name = "label"
@@ -37,9 +33,9 @@ label.Parent = holder
 label.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 label.BackgroundTransparency = 1.000
 label.BorderSizePixel = 0
-label.Position = UDim2.new(0.356913179, 0, 0, 0)
-label.Size = UDim2.new(0, 200, 0, 47)
-label.Font = Enum.Font.GothamBold
+label.Position = UDim2.new(0.214972213, 0, 0.0263160504, 0)
+label.Size = UDim2.new(0, 135, 0, 37)
+label.Font = Enum.Font.Unknown
 label.Text = "FPS"
 label.TextColor3 = Color3.fromRGB(255, 255, 255)
 label.TextScaled = true
@@ -48,7 +44,7 @@ label.TextWrapped = true
 
 -- Scripts:
 
-local function JTQU_fake_script() -- label.LocalScript 
+local function APHL_fake_script() -- label.LocalScript 
 	local script = Instance.new('LocalScript', label)
 
 	--Created by UraniumBanana5
@@ -68,4 +64,4 @@ local function JTQU_fake_script() -- label.LocalScript
 	end
 	
 end
-coroutine.wrap(JTQU_fake_script)()
+coroutine.wrap(APHL_fake_script)()
