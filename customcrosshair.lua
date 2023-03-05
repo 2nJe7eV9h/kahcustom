d86a4c9da9f988883744d16661c51070ee2b2e69
AnonymousCoder <76236098+2nJe7eV9h@users.noreply.github.com>
2023-03-05 23:34:00 +0000

    Update customcrosshair.lua

--- customcrosshair.lua
+++ customcrosshair.lua
@@ -5,7 +5,6 @@
 
 local a = Instance.new("ScreenGui")
 local ImageLabel = Instance.new("ImageLabel")
-local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 
 --Properties:
 
@@ -16,8 +15,6 @@ a.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 ImageLabel.Parent = a
 ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageLabel.BackgroundTransparency = 1.000
-ImageLabel.Position = UDim2.new(0, 600, 0, 399)
+ImageLabel.Position = UDim2.new(0.5, -10, 0.5, -10)
 ImageLabel.Size = UDim2.new(0, 20, 0, 20)
 ImageLabel.Image = "rbxassetid://10830675223"
-
-UIAspectRatioConstraint.Parent = ImageLabel
