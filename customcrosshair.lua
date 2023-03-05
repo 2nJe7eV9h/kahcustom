841f1708ec76abfe8f0d7fcd37abe0592487bd39
AnonymousCoder <76236098+2nJe7eV9h@users.noreply.github.com>
2023-03-05 23:39:01 +0000

    Update customcrosshair.lua

--- customcrosshair.lua
+++ customcrosshair.lua
@@ -15,4 +15,4 @@ ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageLabel.BackgroundTransparency = 1.000
 ImageLabel.Position = UDim2.new(0.5, -10, 0.5, -10)
 ImageLabel.Size = UDim2.new(0, 20, 0, 20)
-ImageLabel.Image = "rbxassetid://10830675223"
+ImageLabel.Image = "rbxassetid://nil"
